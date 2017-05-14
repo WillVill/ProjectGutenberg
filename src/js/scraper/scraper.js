be29df5a54d68a8a2417fc7e89ea10305a135b1c
LukaszKoziarski <lkoziars@gmail.com>
2017-05-15 00:13:22 +0200

    Synchronous scraper

--- src/js/scraper/scraper.js
+++ src/js/scraper/scraper.js
@@ -1,58 +1,59 @@
 var fs = require('fs');
 var utils = require('./utils.js');
-var title;
-var author;
-var citiesArr = [];
+var colors = require('colors');
 
 // returns an array of strings (potential cities) from the given book
-function getBookStrings(bookFile, callback) {
-    var readStream = fs.createReadStream(bookFile, 'utf8');
-    var book = '';
+function getPotentialCitiesFromBook(book) {
+    // remove Gutenberg footer which is the same in every book
+    var licenceCrap = `*** END OF THE PROJECT GUTENBERG EBOOK`;
+    var licenceIndex = book.indexOf(licenceCrap);
+    if (licenceIndex !== -1) {
+        // grab everything what's before
+        book = book.substring(0, licenceIndex);
+    }
 
-    readStream.on('data', function(chunk) {  
-        book += chunk;
-    }).on('end', function() {
-        var regex = /[A-Z]+(?:[\s]?)(?:[\.]?[\s]?)?[A-Za-z. ]*/gm;
-        var data = book.match(regex).join("|");
+    // regular expression for all the potential cities
+    var regex = /[A-Z]+(?:[\s]?)(?:[\.]?[\s]?)?[A-Za-z. ]*/gm;
+    // get potential cities and join them in one big string
+    var potentialCities = book.match(regex).join("|");
 
-        title = getTitle(book);
-        author = getAuthor(book);
-
-        callback(data);
-    });
+    return potentialCities;
 }
 
-// reuturns an array of city objects;
-function getCities(callback) {
-    fs.readFile('/Users/Luke/Desktop/ProjectGutenberg/scraped_cities/scraped_cities5000.txt', 'utf8', function(err, cities) {
-        if (err) throw err;
-
-        var cityJSON = JSON.parse(cities);
-        callback(cityJSON);
-    });
+function getCities() {
+    var cities = fs.readFileSync('../../../scraped_cities/scraped_cities15000.txt', 'utf8');
+    var cityJSON = JSON.parse(cities);
+    return cityJSON;
 }
 
-function getCitiesFromBook(bookFile) {
-    getBookStrings(bookFile, (bookStrings) => {
-        getCities(cities => {
+function scrapeBook(bookFile, cities) {
+    var book = fs.readFileSync(bookFile, 'utf8');
+    var potentialCities = getPotentialCitiesFromBook(book);
+    var dir = "../../../";
+    var geos = '';
 
-            for (var j = cities.length - 1; j >= 0; j--) {
-                if (bookStrings.indexOf(cities[j].name + " ") !== -1) {
-                    citiesArr.push(cities[j]);
-                }  
+    var length = cities.length;
+    cities.forEach((city, index) => {
+        // Check if the city is included inside of the potential cities string
+        // Space in the end prevents from catching cities as parts of some other words. Nothing in a begining since a sentence could start from a city name.
+        if (potentialCities.indexOf(city.name + " ") !== -1) {
+            if (index === length - 1) {
+                geos += parseInt(city.geo);
+            } else {
+                geos += parseInt(city.geo) + '|';                
             }
-
-            var dataFromBook = createData();
-            var dir = "../../../";
-
-            utils.saveToFile(dataFromBook, dir, "book_data", "csv");
-        });
+        }   
     });
-}
 
-// getCitiesFromBook('/Users/Luke/Desktop/testing_files/238.txt');
-// scrapeCities('/Users/Luke/Desktop/cities15000.txt');
-scrapeAllBooks();
+    var title = getTitle(book);
+    var author = getAuthor(book);
+    // substring stripe depends on the length of the dir path - requires change if dir is different from `/Users/Luke/Desktop/books_part_1/`
+    var fileName = bookFile.substring(33, bookFile.length);
+
+    var bookMetadata = combineBookData(fileName, title, author, geos);
+
+    utils.saveToFile(bookMetadata, dir, "book_data", "csv");
+}
 
 // scrapes city name and its geolocation from the given file
 function scrapeCities(citiesFile) {
@@ -65,15 +66,18 @@ function scrapeCities(citiesFile) {
         var arrLines = cities.split('\n');
 
         for (var i = 0; i < arrLines.length; i++) {
-            city = arrLines[i].split(/\t/, 3);
-
+            city = arrLines[i].split(/\t/, 9);
             arrCities.push({
+                geo: city[0],
                 name: city[2],
-                geo: city[0]
+                asciiName: city[1],
+                longitude: city[5],
+                latitude: city[4],
+                countryCode: city[8]
             });
         }
 
-        utils.saveToFile(arrCities, "/Users/Luke/Desktop/ProjectGutenberg/scraped_cities/", "scraped_cities15000", "csv");
+        utils.saveToFile(arrCities, "../../../scraped_cities/", "scraped_cities15000", "txt");
     });
 }
 
@@ -90,7 +94,7 @@ function getTitle(book) {
             })
             .join(" ");    
     }
-    return "UNKNOWN";
+    return "Unknown";
 }
 
 function getAuthor(book) {
@@ -99,30 +103,41 @@ function getAuthor(book) {
     if (authorArr) {
         return authorArr[0].substring(8);
     }
-    return "UNKNOWN";
+    return "Unknown";
 }
 
-function createData() {
+function combineBookData(fileName, title, author, geos) {
     var arr = [];
-    for (var i = citiesArr.length - 1; i >= 0; i--) {
-        arr.push({
-            title: title,
-            author: author,
-            cityGeo: citiesArr[i].geo
-        });
-    }
+
+    arr.push({
+        fileName: fileName,
+        title: title,
+        author: author,
+        cityGeo: geos
+    });
 
     return arr;
 }
 
-function scrapeAllBooks() {
-    fs.readdir('/Users/Luke/Desktop/testing_files/', (err, files) => {
-        // in this case for loop skips the last file which is the .DS_Store file - it stores custom attributes of its containing folder
-        for (var i = files.length - 1; i >= 1; i--) {
-            console.log(files[i]);
-            getCitiesFromBook('/Users/Luke/Desktop/testing_files/' + files[i]);
-        }
+function scrapeBooks(dirWithBooks) {
+    // get collection of cities
+    var cities = getCities();
+    // get collection of book files
+    var files = fs.readdirSync(dirWithBooks);
+    // skip the .DS_Store file - it stores custom attributes of its containing folder
+    files.splice(0, 1);
+
+    var amountOfFiles = files.length;
+    var i = 1;
+
+    files.forEach(file => {
+        console.time("Time");
+        console.log("\nProcessing file: " + file.yellow + "\t" + colors.cyan(i + " out of " + amountOfFiles) + "\t" + utils.getProgressPercentage(i, amountOfFiles).magenta);
+        scrapeBook(dirWithBooks + file, cities);
+        console.timeEnd("Time");
+        i++;
     });
 }
 
-// geolocation: arrLines[i].match(/^\d+|\d+\b|\d+/).shift()
+// scrapeCities('/Users/Luke/Desktop/cities15000.txt');
+scrapeBooks('/Users/Luke/Desktop/books_part_2/');
