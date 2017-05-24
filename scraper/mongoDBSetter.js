8232470e5787841de6269ac6f0221f188f844fd8
LukaszKoziarski <lkoziars@gmail.com>
2017-05-24 15:19:31 +0200

    changed model for City

--- scraper/mongoDBSetter.js
+++ scraper/mongoDBSetter.js
@@ -1,19 +1,22 @@
 var fs = require('fs');
-var mongoose = require('mongoose');
 var config = require('../config.js');
 var Papa = require('papaparse');
+
+var mongoose = require('mongoose');
 var bookSchema = require('../server/models/book.js').Book;
 var Book = mongoose.model('Book', bookSchema);
+var citySchema = require('../server/models/city.js').City;
+var City = mongoose.model('City', citySchema);
 
-function connect() {
+function getConnection() {
     mongoose.connect(config.db.mongodb);
+    return mongoose.connection;
 }
 
 function addBooksToDB() {
     // get scraped cities from all the books (fileName,title,author,geos)
     var bookMetadata = fs.readFileSync('../book_data.csv', 'utf8');
-    connect();
-    var db = mongoose.connection;
+    var db = getConnection();
 
     Papa.parse(bookMetadata, {
         header: true,
@@ -47,6 +50,38 @@ function addBooksToDB() {
  *  mongoimport --drop --db mongoTest --collection cities --type csv --headerline --file /Users/Luke/Desktop/ProjectGutenberg/scraped_cities/scraped_cities5000.csv
  */
 
+function addCitiesToDB() {
+    var citiesMetadata = fs.readFileSync('../scraped_cities/scraped_cities5000.csv', 'utf8');
+    var db = getConnection();
+
+    Papa.parse(citiesMetadata, {
+        header: true,
+        dynamicTyping: true,
+        step: function(row) {
+            var arr = [];
+
+            var cityDoc = new City({
+                geo: row.data[0].geo,
+                name: row.data[0].name,
+                asciiName: row.data[0].asciiName,
+                countryCode: row.data[0].countryCode,
+            });
+
+            arr.push(row.data[0].longitude);
+            arr.push(row.data[0].latitude);
+            cityDoc.loc = arr;
+
+            cityDoc.save(function (err, cityDoc) {
+              if (err) return console.error(err);
+            });
+        },
+
+        complete: function() {
+            db.close();
+        }
+    });
+ }
+
 function stringIntoArray(geoString) {
     var arr = geoString.split('|').slice(0, -1);
     var arrObj = [];
@@ -60,5 +95,5 @@ function stringIntoArray(geoString) {
     return arrObj;
 }
 
-addBooksToDB();
+addCitiesToDB();
 
